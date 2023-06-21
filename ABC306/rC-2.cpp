7b56dfacab59c2ca269dfb0906f535290e1d325f
Ponnosuke <ponnosuke@MyComputer>
2023-06-22 02:16:42 +0900

    Practice20230622

---
+++ ABC306/rC-2.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = int64_t;
+using ui64 = uint64_t;
+
+#ifdef LOCAL_DEBUG
+    #define debug(X) std::cerr << X << " "
+    #define debugV(X) std::cerr << #X << " = " << X << ", "
+    #define debugVL(Y) std::cerr << #Y << " = " << Y << '\n'
+    #define debugWL(Z) std::cerr << #Z << '\n'
+    #define debugP() std::cerr << "Line:" << __LINE__ << '\n'
+#else
+    #define debug(X)
+    #define debugV(X)
+    #define debugVL(Y)
+    #define debugWL(Z)
+    #define debugP()
+#endif
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(3*N, 0);
+    for(int i = 0; i < 3*N; i++){
+        cin >> A.at(i);
+    }
+
+    vector<int> cnt(N+1, 0);
+    vector<int> ans;
+    for(int i = 0; i < 3*N; i++){
+        int nowA = A.at(i);
+
+        cnt.at(nowA)++;
+        if(cnt.at(nowA) == 2){
+            ans.push_back(nowA);
+        }
+    }
+
+    for(int i = 0; i < (int)ans.size(); i++){
+        cout << ans.at(i);
+
+        if(i < (int)ans.size() - 1){
+            cout << " ";
+        }
+    }
+    cout << '\n';
+}
